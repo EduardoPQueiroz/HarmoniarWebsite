a0c12c6f457847fa9403173fa958148ca71b6a0c
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2025-06-02 23:26:08 -0300

    feat: Starting footer structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
