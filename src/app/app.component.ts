0aa51119b6d791ece216e122f013d4c626ecc730
lary17181 <139398675+lary17181@users.noreply.github.com>
2025-06-10 22:04:29 -0300

    Missão e Valores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MVVComponent } from './mvv/mvv.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, HomeComponent],
+  imports: [FooterComponent, HomeComponent, MVVComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/mvv/mvv.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mvv',
+  standalone:true,
+  templateUrl: './mvv.component.html',
+  styleUrls: ['./mvv.component.css']
+})
+export class MVVComponent {
+
+}
