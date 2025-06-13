97fb8041ae3ab5e721a1c70fc8d84bbbc3b2d338
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2025-06-13 11:09:55 -0300

    routes structure ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, HomeComponent, HeaderComponent],
+  imports: [FooterComponent, RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
 
+import { HomeComponent } from './app/home/home.component';
+// import { SobreNosComponent } from './app/sobre-nos/sobre-nos.component';
+// import { MissaoValoresComponent } from './app/missao-valores/missao-valores.component';
+// import { NossoAplicativoComponent } from './app/nosso-aplicativo/nosso-aplicativo.component';
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', component: HomeComponent },
+      // { path: 'sobre-nos', component: SobreNosComponent },
+      // { path: 'missao-valores', component: MissaoValoresComponent },
+      // { path: 'nosso-aplicativo', component: NossoAplicativoComponent },
+      { path: '**', redirectTo: '' }
+    ])
+  ]
+});
