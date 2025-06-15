68e75e7fbef4d0097733cf7351532d9dc9e23462
MATHEUS VIEIRA <matheus.vieira137@etec.sp.gov.br>
2025-06-15 14:55:20 -0300

    feat: Finished AboutUs Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, HomeComponent],
+  imports: [FooterComponent, HomeComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
