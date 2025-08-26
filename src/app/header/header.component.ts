ddf1e0b3f9a4388ff41c2f90e51b39d31312e6e9
unknown <limajeffersonn654@gmail.com>
2025-08-26 12:31:06 -0300

    botão menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,18 +10,9 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-isMenuOpen = false;
-
-b="";
-
+isMenuOpen=true;
   toggleMenu() {
-    console.log("socorroo");
     this.isMenuOpen=!this.isMenuOpen;
-    if(this.isMenuOpen==true){
-      this.b="mobile-menu";
-    }
-    else{
-      this.b="";
-    }
+
   }
 }
